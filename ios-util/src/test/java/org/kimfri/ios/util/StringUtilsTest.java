6fac4b6e6af620b2eba056952f18d5d47dae08fe
Kim Fritzon-Ölander <kim.fritzon@gmail.com>
2023-11-04 15:54:59 +0100

    Refactor all tests
    By using parameterized test one can test more things in one go.
    This makes the code more condensed,

--- ios-util/src/test/java/org/kimfri/ios/util/StringUtilsTest.java
+++ ios-util/src/test/java/org/kimfri/ios/util/StringUtilsTest.java
@@ -2,12 +2,16 @@ package org.kimfri.ios.util;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class StringUtilsTest {
 
@@ -17,36 +21,34 @@ class StringUtilsTest {
 
   @Nested
   class Concatenate {
-    @Test
-    void givenNullAsList_thenReturnNull() {
-      assertNull(StringUtils.concatenate(null, ""));
+    @ParameterizedTest
+    @MethodSource("possibleNullValues")
+    void givenAnyArgumentIsNull_thenReturnNull(List<String> stringList, String delimiter) {
+      assertNull(StringUtils.concatenate(stringList, delimiter));
     }
 
-    @Test
-    void givenNullAsDelimiter_thenReturnNull() {
-      assertNull(StringUtils.concatenate(List.of(""), null));
+    @ParameterizedTest
+    @MethodSource("nonNullValues")
+    void givenNonNullValues_CorrectValueIsReturned(List<String> stringList,
+                                                   String delimiter,
+                                                   String expected) {
+      assertEquals(StringUtils.concatenate(stringList, delimiter), expected);
     }
 
-    @Test
-    void givenNullForBothArguments_thenReturnNull() {
-      assertNull(StringUtils.concatenate(null, null));
-    }
-
-    @Test
-    void givenOneString_thenReturnStringWithoutDelimiter() {
-      String content = "abc";
-      String delimiter = ",";
-      assertEquals(content, StringUtils.concatenate(List.of(content), delimiter));
-    }
-
-    @Test
-    void givenTwoStrings_thenReturnStringsWithDelimiter() {
-      final String first = "abc";
-      final String second = "def";
-      final List<String> list = List.of(first, second);
+    private static Stream<Arguments> nonNullValues() {
       final String delimiter = ",";
-      final String expected = first + delimiter + second;
-      assertEquals(expected, StringUtils.concatenate(list, delimiter));
+      return Stream.of(
+          arguments(List.of("abc", "def"), delimiter, "abc,def"),
+          arguments(List.of("abc"), delimiter, "abc")
+      );
+    }
+
+    private static Stream<Arguments> possibleNullValues() {
+      return Stream.of(
+          arguments(null, null),
+          arguments(null, ","),
+          arguments(List.of("abc"), null)
+      );
     }
   }
 }
